14645af9f57e234835b5543900398f8ac651aea5
HARIPRASAD <152404369+HPMISHRAA@users.noreply.github.com>
2024-03-13 21:47:13 +0530

    Add files via upload

---
+++ Number/Happy Number/HappyNumber.java
@@ -0,0 +1,25 @@
+package com.practice;
+
+public class HappyNumber {
+	public static void main(String[] args) {
+		int no = 13;
+		int copy = no;
+		while (no > 9) {
+			no = sum(no);
+		}
+		if (no == 1 || no == 7) {
+			System.out.println(copy + "----> Is a Happy Number");
+		} else {
+			System.out.println(copy + "----> Is Not a Happy Number");
+		}
+	}
+	static int sum(int no) {
+		int sum = 0;
+		while (no != 0) {
+			int rem = no % 10;
+			sum += (rem * rem);
+			no = no / 10;
+		}
+		return sum;
+	}
+}
